1072f5d1453d8d6091ad8966f2bcb2dbe670f7fc
Snow <yeunghuanyuan@qq.com>
2024-11-07 21:41:40 +0800

    feat: 2.9

--- src/repl.js
+++ src/repl.js
@@ -1,20 +1,55 @@
 import { createInterface } from 'readline'
 import { Lexer } from './lexer.js'
-import { TokenType } from './token.js'
+import { Parser } from './parser.js'
 
-export const startRepl = (inputStream = process.stdin, outputStream = process.stdout) => {
+const MONKEY_FACE = `            
+            __,__
+    .--. .-"     "-. .--.
+  /.. \/ .-..-.  \/.. \ 
+ | |  '|  /   Y   \  |'  | |
+ | \   \\  \\ 0 | 0 /  /   / |
+  \\ '-,\\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\\ '-''
+       |  \\._   _./  |
+       \\   \\ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
+const printParserErrors = (outputStream, errors) => {
+  outputStream.write(MONKEY_FACE)
+  outputStream.write('Woops! We ran into some monkey business here!\n')
+  outputStream.write(' parser errors:\n')
+  errors.forEach((msg) => outputStream.write('\t' + msg + '\n'))
+}
+
+export const startRepl = (
+  inputStream = process.stdin,
+  outputStream = process.stdout
+) => {
   const PROMPT = '>> '
-  const rl = createInterface({ input: inputStream, output: outputStream, prompt: PROMPT })
+  const rl = createInterface({
+    input: inputStream,
+    output: outputStream,
+    prompt: PROMPT,
+  })
 
   rl.prompt()
 
   rl.on('line', (line) => {
     const lexer = new Lexer(line)
-    let token = lexer.nextToken()
-    while (token.type !== TokenType.EOF) {
-      outputStream.write(`${JSON.stringify(token)}\n`)
-      token = lexer.nextToken()
+    const parser = new Parser(lexer)
+    const program = parser.parseProgram()
+
+    if (parser.getErrors().length !== 0) {
+      printParserErrors(outputStream, parser.getErrors())
+      rl.prompt()
+      return
     }
+
+    outputStream.write(program.getString())
+    outputStream.write('\n')
+
     rl.prompt()
   })
 
